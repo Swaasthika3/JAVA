052b2429721718b29a49697aef42a3ae953ea042
Swaasthika3 <168736797+Swaasthika3@users.noreply.github.com>
2025-07-31 15:23:14 +0530

    Create strongno.java

---
+++ strongno.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int copy=n;
+        int fact=1,sum=0;
+        while(n>0){
+            int r=n%10;
+            n=n/10;
+            fact=1;
+            for(int i=1;i<=r;i++){
+                fact=fact*i;
+            }
+            sum=sum+fact;
+        }
+        n=copy;
+        if(sum==copy){
+            System.out.println("Strong Number");
+        }
+        else
+             System.out.println("Not a strong number");
+    }
+}
